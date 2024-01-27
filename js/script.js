25a20d16120c9f97e3720521c87d61fb8a9e1a46
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-01-27 22:36:03 +0500

    Files uploaded

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// BY GHc 
+
+
+// console.log("hello world");
+
+
+
+document.querySelector(".serviceli").addEventListener("mouseenter", (e) => {
+    console.log(e.target)
+    e.target.querySelector("a").setAttribute("style", "background:white; color:darkmagenta")
+    e.target.querySelector("ul").setAttribute("style", "display:flex !important")
+})
+document.querySelector(".serviceli").addEventListener("mouseleave", (e) => {
+    e.target.querySelector("a").setAttribute("style", "background:blue; color:white")
+    e.target.querySelector("ul").setAttribute("style", "display:none !important")
+})
